b82cc43bcd8a9d2dd1cd02ffcae7bef250c947d3
Mahak <mahakhardiya@gmail.com>
2024-03-09 16:57:53 +0530

    router config

--- classroom/routes/post.js
+++ classroom/routes/post.js
@@ -2,19 +2,19 @@ const express = require("express");
 const router = express.Router();
 
 router.get("/", (req, res)=>{
-    res.send("Get for users");
+    res.send("GET for posts");
 });
 
 router.get("/:id", (req, res)=>{
-    res.send("Get for users id");
+    res.send("GET for post id");
 });
 
 router.post("/", (req, res)=>{
-    res.send("Post for users");
+    res.send("POST for posts");
 });
 
 router.delete("/:id", (req, res)=>{
-    res.send("Delete for users id");
+    res.send("DELETE for posts id");
 });
 
 module.exports = router;
