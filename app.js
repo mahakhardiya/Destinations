df99998a16def2ccdf70cfa7e71ba5d09cab5885
Mahak <mahakhardiya@gmail.com>
2024-03-27 21:55:51 +0530

    automatic login and redirect to original url

--- app.js
+++ app.js
@@ -63,7 +63,7 @@ passport.deserializeUser(User.deserializeUser()); // Termination of user related
 app.use((req, res, next)=>{
     res.locals.success = req.flash("success");
     res.locals.error = req.flash("error");
-    console.log(res.locals.success);
+    res.locals.currUser = req.user;
     next();
 });
 
