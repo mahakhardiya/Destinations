ba704da638fac0227dae1cc214da1119ffd558ab
Mahak <mahakhardiya@gmail.com>
2024-03-31 15:28:09 +0530

    MVC- Controller added

--- routes/review.js
+++ routes/review.js
@@ -1,34 +1,14 @@
 const express = require("express");
 const router = express.Router({mergeParams : true});
 const wrapAsync = require("../utils/wrapAsync.js");
-const Review = require("../models/review.js");
-const Listing = require("../models/listing.js");
 const {isReviewAuthor, isLoggedIn, validateReview} = require("../middleware.js");
 
+const reviewContoller = require("../controllers/review.js");
+
 //post review route
-router.post("/", isLoggedIn, validateReview, wrapAsync(async (req, res) => {
-    let listing = await Listing.findById(req.params.id);
-    let newReview = new Review(req.body.review);
-    newReview.author = req.user._id;   
-    listing.reviews.push(newReview);
-    await newReview.save();
-    await listing.save();
-    req.flash("success", "New Review Created!");
-    console.log("New review added");
-    res.redirect(`/listings/${listing._id}`);
- })
- );
+router.post("/", isLoggedIn, validateReview, wrapAsync(reviewContoller.createReview));
 
 //delete review route
-router.delete("/:reviewId", isReviewAuthor, isLoggedIn , wrapAsync(async (req, res) => {
-    let { id, reviewId } = req.params;
-
-    await Listing.findByIdAndUpdate(id, {$pull: {reviews: reviewId}})
-
-    await Review.findByIdAndDelete(reviewId);
-    req.flash("success", "Review Deleted!");
-    res.redirect(`/listings/${id}`);
-})
-);
+router.delete("/:reviewId", isReviewAuthor, isLoggedIn , wrapAsync(reviewContoller.deleteReview));
 
 module.exports = router;
