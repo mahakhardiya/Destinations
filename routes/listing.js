2b56c05522a0c983e3d0ca98c18ed5c271f5649f
Mahak <mahakhardiya@gmail.com>
2024-04-04 00:02:31 +0530

    multer-storage-cloudinary/ cloudConfig

--- routes/listing.js
+++ routes/listing.js
@@ -4,14 +4,14 @@ const wrapAsync = require("../utils/wrapAsync.js");
 const { isLoggedIn, isOwner, validateListing } = require("../middleware.js");
 const listingController = require("../controllers/listing.js");
 const multer  = require('multer');
-// const upload = multer({ dest: 'uploads/' });
-
+const {storage} = require("../cloudConfig.js");
+const upload = multer({ storage });
 
 router
     .route("/")
     .get(wrapAsync(listingController.index)) //index route
-    .post(isLoggedIn, validateListing, wrapAsync(listingController.createListing)); //create route
-
+    .post(isLoggedIn, validateListing, upload.single("listing[image]"), wrapAsync(listingController.createListing)); //create route
+ 
 //new route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
