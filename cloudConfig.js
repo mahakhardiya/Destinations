22ffee8b051f4d154707ecde15e10f918db31374
Mahak <mahakhardiya@gmail.com>
2024-04-08 21:31:31 +0530

    Timestamp

--- cloudConfig.js
+++ cloudConfig.js
@@ -7,11 +7,18 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 });
 
+function generateTimestamp() {
+    return Math.round(new Date().getTime() / 1000);
+}
+
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: {
-      folder: 'destinations_DEV',
-      allowedformats:["png", "jpg", "jpeg"],
+    params: (req, file) => {
+        return {
+            folder: 'destinations_DEV',
+            allowed_formats: ['png', 'jpg', 'jpeg'], // Add allowed formats here
+            timestamp: generateTimestamp()
+        };
     },
 });
 
