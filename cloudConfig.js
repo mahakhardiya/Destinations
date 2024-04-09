67d0f51558c11d723cea2140eb1ea0976ea02edb
Mahak <mahakhardiya@gmail.com>
2024-04-09 22:38:58 +0530

    Testing

--- cloudConfig.js
+++ cloudConfig.js
@@ -1,23 +1,28 @@
 const cloudinary = require('cloudinary').v2;
 const { CloudinaryStorage } = require('multer-storage-cloudinary');
 
+// Set up Cloudinary configuration
 cloudinary.config({
     cloud_name: process.env.CLOUD_NAME,
     api_key: process.env.CLOUD_API_KEY,
     api_secret: process.env.CLOUD_API_SECRET
 });
 
+// Function to generate a timestamp
 function generateTimestamp() {
+    // Use the current time as the timestamp
     return Math.round(new Date().getTime() / 1000);
 }
 
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
     params: (req, file) => {
+        const timestamp = generateTimestamp(); // Generate timestamp
         return {
             folder: 'destinations_DEV',
             allowed_formats: ['png', 'jpg', 'jpeg'], // Add allowed formats here
-            timestamp: generateTimestamp()
+            timestamp: timestamp, // Use generated timestamp
+            signature: cloudinary.utils.api_sign_request({ timestamp: timestamp }, process.env.CLOUD_API_SECRET) // Generate signature
         };
     },
 });
