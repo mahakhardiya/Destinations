84d37ea876193446f6671a11a452a725286adc9b
Mahak <mahakhardiya@gmail.com>
2024-04-09 23:28:18 +0530

    Testing

--- cloudConfig.js
+++ cloudConfig.js
@@ -8,30 +8,13 @@ cloudinary.config({
     api_secret: process.env.CLOUD_API_SECRET
 });
 
-// Function to generate a timestamp
-function generateTimestamp() {
-    // Use the current time as the timestamp
-    return Math.round(new Date().getTime() / 1000);
-}
-
 const storage = new CloudinaryStorage({
     cloudinary: cloudinary,
-    params: async () => {
-        const timestamp = generateTimestamp(); // Generate timestamp
-        const folder = 'destinations_DEV'; // Define the folder parameter
-
-        return {
-            folder: folder,
-            allowed_formats: ['png', 'jpg', 'jpeg'], // Add allowed formats here
-            timestamp: timestamp, // Use generated timestamp
-            signature: cloudinary.utils.api_sign_request({
-                timestamp: timestamp,
-                folder: folder,
-                api_key: process.env.CLOUD_API_KEY
-            }, process.env.CLOUD_API_SECRET) // Generate signature
-        };
+    params: {
+      folder: 'destinations_DEV',
+      allowedFormats: ["png", "jpg", "jpeg"], // supports promises as well
     },
-});
+  });
 
 module.exports = {
     cloudinary,
